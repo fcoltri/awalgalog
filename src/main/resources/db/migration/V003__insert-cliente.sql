94833cc5d52fe906882b4f71385ff304a9ee00af
Fernando Coltri <fcoltri@gmail.com>
2022-03-12 20:36:29 -0300

    Mapeamento classe cliente e populando tabela cliente no BD

---
+++ src/main/resources/db/migration/V003__insert-cliente.sql
@@ -0,0 +1,2 @@
+insert into cliente (nome, email, tel) values ("Fernando", "fernando@gmail.com", "(11) 99991-2345");
+insert into cliente (nome, email, tel) values ("Cristiane", "cris@gmail.com", "(11) 98765-2345");
